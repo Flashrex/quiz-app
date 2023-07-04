48ab693bd3ca8231b96b8330637a5162aa898ab6
DominikDuda <Dominik.Duda96@gmx.de>
2023-07-04 21:17:03 +0200

    load quizes from json

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,34 @@
 <script setup lang="ts">
 
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import Quiz from './components/Quiz.vue';
 import Home from './components/Home.vue';
 import { type IQuiz } from './types/quiz';
+import axios from 'axios';
+import { quizNames } from './quizzes';
 
-const quizes = ref<IQuiz[]>(
-  [
-    {
-      title: "Vue.js Grundkurs Fragen",
-      questions: [
-        { type: "multiple_choice", title: "Was ist Vue.js?", answers: [{ text: "Datenbanksystem", isCorrect: false }, { text: "Programmiersprache", isCorrect: false }, { text: "JavaScript Framework", isCorrect: true }] },
-        { type: "multiple_choice", title: "Welches Unternehmen steckt hinter Vue.js?", answers: [{ text: "Facebook", isCorrect: false }, { text: "Google", isCorrect: false }, { text: "Keines (Open Source Community)", isCorrect: true }] },
-        { type: "single_choice", title: "Welchen API Stil sollte man für eine vollständige Applikation verwenden?", answers: [{ text: "Options API", isCorrect: false }, { text: "Composition API", isCorrect: true }] },
-        { type: "drop_list", title: "Bringe die Elemente in die richtige Reihenfolge", answers: [{ text: "Answer A" }, { text: "Answer B" }, { text: "Answer C" }, { text: "Answer D" }, { text: "Answer E" }] },
-      ]
-    },
-    {
-      title: "Test 2",
-      questions: [
-        { type: "multiple_choice", title: "What is the answer?", answers: [{ text: "Answer 1", isCorrect: false }, { text: "Answer 2", isCorrect: false }, { text: "Answer  3", isCorrect: false }] },
-        { type: "single_choice", title: "What is the answer?", answers: [{ text: "Answer 1", isCorrect: false }, { text: "Answer 2", isCorrect: false }, { text: "Answer  3", isCorrect: false }] }
-      ]
+const quizzes = ref<IQuiz[]>([]);
+
+onMounted(async () => {
+  let data: IQuiz[] = [];
+
+  try {
+
+    for (const quiz of quizNames) {
+      const fileResponse = await axios.get(`assets/quizzes/${quiz}.json`);
+      const fileData = fileResponse.data;
+      data.push(fileData);
     }
-  ]
-);
+  } catch (err) {
+    console.error(err);
+  }
+  quizzes.value = data;
+})
 
 const selectedQuiz = ref<IQuiz>();
 
 function onSelectedQuiz(index: number) {
-  selectedQuiz.value = quizes.value[index];
+  selectedQuiz.value = quizzes.value[index];
 }
 
 function onReturnToMain() {
@@ -40,7 +39,7 @@ function onReturnToMain() {
 
 <template>
   <main>
-    <Home v-if="!selectedQuiz" :quizzes="quizes" @selected-quiz="onSelectedQuiz"></Home>
+    <Home v-if="!selectedQuiz" :quizzes="quizzes" @selected-quiz="onSelectedQuiz"></Home>
     <Quiz v-else :quiz="selectedQuiz" @return-to-main="onReturnToMain"></Quiz>
   </main>
 </template>
---
+++ src/quizzes.ts
@@ -0,0 +1,5 @@
+
+export const quizNames = [
+    "vue-quiz",
+    "" //add name of file of your new quiz
+]
